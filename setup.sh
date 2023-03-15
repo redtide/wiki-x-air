c0c4689a712c8f250c73d47b01af4eeb69d64d2b
redtide <redtid3@gmail.com>
2023-03-15 12:33:36 +0100

    Updated site config

--- setup.sh
+++ setup.sh
@@ -1,27 +1,39 @@
+#!/bin/bash
+
 cd $PWD
 
-if [ "$1" == "--help" ] ||  [ "$1" == "-h" ]; then
-	echo "Usage: setup [option]"
+set -e
+
+script_name=$(basename $0 | sed "s/\.sh$//")
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
 	echo "Setup and run Jekyll"
+	echo ""
+	echo "Usage: ${script_name} [option]"
 	echo ""
 	echo "Options are not mandatory, only one at a time."
 	echo "-a, --assets      Build minimized css style and js script from sources."
 	echo "-i, --install     Install Bundler and node modules using Yarn."
+	echo "-I, --incremental Runs Jekyll in incremental mode."
+	echo ""
 	exit 0
 fi
 
+mkdir -p assets/css assets/js
+
 if [ ! -d "node_modules" ] || [ "$1" == "-i" ] || [ "$1" == "--install" ]; then
 	gem update
-	echo "Installing Bundler..."
 	gem install bundler
-	echo "Running Yarn install..."
+	bundle install
 	yarn --no-bin-links
 fi
 
 if [ ! -f "assets/css/style.min.css" ] || [ "$1" == "-a" ] || [ "$1" == "--assets" ]; then
-	echo "Running Yarn dist..."
 	yarn dist
 fi
 
-echo "Running Jekyll..."
-bundle exec jekyll serve --watch --host=0.0.0.0
+if [ "$1" == "-I" ] || [ "$1" == "--incremental" ]; then
+	incremental="-I"
+fi
+
+bundle exec jekyll serve --watch --host=0.0.0.0 $incremental
